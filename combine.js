f433d061d03f73f2491ac2c22439c3ecf8e2e8ec
Ryan Cooper <ryan.cooper@groupbyinc.com>
2019-08-09 12:40:15 -0400

    Added source army to the name when in "all"

--- combine.js
+++ combine.js
@@ -8,14 +8,15 @@ let count = 0;
 fs.readdir("./", (err, files) => {
 	files = files.filter((file) => {
 		const broken = file.split(".");
-		return broken[broken.length-1] === 'txt' && broken[broken.length-1] !== 'all';
+		return broken[broken.length-1] === 'txt' && broken[broken.length-2] !== 'all';
 	});
 	files.forEach(file => {
 		fs.readFile(`./${file}`, 'utf8', (err, data) => {
 			const parsed = JSON.parse(base64.decode(data));
+			const broken = file.split(".");
 			count ++;
 			Object.keys(parsed).forEach((item) => {
-				allData[item] = parsed[item];
+				allData[`${item} (${broken[broken.length-2].split("_").join(" ")})`] = parsed[item];
 			});
 			if(count === files.length) {
 				const output = base64.encode(JSON.stringify(allData));
